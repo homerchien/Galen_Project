c2db2b02e56f9f1a403a34a6fa244e49598de84f
Homer <homerchien0224@gmail.com>
2018-12-26 11:01:39 +0800

    add js method

--- specs/galen-extras/custom-javascript-functions.js
+++ specs/galen-extras/custom-javascript-functions.js
@@ -1,32 +1,31 @@
-this.allEven = function (pattern,aaa) {
+this.allEven = function (pattern) {
     var size = count(pattern);
-    var aaaa = count(aaa);
     var parameters = "2";
     if (size > 1) {
        for (var i = 4; i <= size; i+=2) {
            parameters = parameters + "," + i;
        }
     }
-    System.out.println("pattern:"+pattern+"    aaa:"+aaa);
     return parameters;
 };
 
-// unfinished
-this.resetJson = function (resetJson,targetJson) {
-	System.out.println(JSON.parse(resetJson));
-	System.out.println(JSON.parse(targetJson));
- //    var resetJson_ = JSON.parse(resetJson);
- //    var targetJson_ = JSON.parse(targetJson);
- //    for(let key in targetJson_){
-	// 	resetJson_[key] = targetJson_[key];
-	// }
+// 更新JSON值
+this.updateJson = function (updateStr,targetStr) {
+    // System.out.println(typeof targetStr);
+    // System.out.println(typeof Object.keys(JSON.parse(targetStr)));
+    var updateJson = JSON.parse(updateStr);
+    var targetJson = JSON.parse(targetStr);
+    for(var key in targetJson){
+        updateJson[key] = targetJson[key];
+        // System.out.println(key);
+        // System.out.println(typeof updateJson[key]);
+    }
+    // System.out.println(JSON.stringify(updateJson));
+    return JSON.stringify(updateJson);
+};
 
- //    // var parameters = "2";
- //    // if (size > 1) {
- //    //    for (var i = 4; i <= size; i+=2) {
- //    //        parameters = parameters + "," + i;
- //    //    }
- //    // }
-
- //    return resetJson_;
+// 获取json长度
+this.getJsonLength = function (json) {
+    // System.out.println(typeof json);
+    return Object.keys(JSON.parse(json)).length;
 };
--- specs/properties/collection-list.properties.js
+++ specs/properties/collection-list.properties.js
@@ -1,4 +1,41 @@
 conf_settings_name = "Collection list";
+conf_settings_grid = "3";
+conf_blocks_a = '["a","b"]';
+conf_blocks = `[{
+    "type": "collection",
+    "name": "autotest_collection_name_lC5CWG3",
+    "settings": {
+      "collection": null
+    }
+  }, {
+    "type": "collection",
+    "name": "Collection",
+    "settings": {
+      "collection": null
+    }
+  }, {
+    "type": "collection",
+    "name": "Collection",
+    "settings": {
+      "collection": null
+    }
+  }, {
+    "type": "collection",
+    "name": "Collection",
+    "settings": {
+      "collection": null
+    }
+  }, {
+    "type": "collection",
+    "name": "Collection",
+    "settings": {
+      "collection": null
+    }
+  }, {
+    "type": "collection",
+    "name": "Collection",
+    "settings": {
+      "collection": null
+    }
+  }]`;
 
-
-	
